156a33d92952085a822670fb55d40fbc04cebed1
Mohd Abdullah <120127558+Abdulgour@users.noreply.github.com>
2025-08-10 18:01:37 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (13.93%) - LeetSync

--- 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.py
+++ 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.py
@@ -1,10 +1,7 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        nums.sort()
         count=0
         for num in nums:
             if num<k:
                 count+=1
-            else:
-                break
         return count
